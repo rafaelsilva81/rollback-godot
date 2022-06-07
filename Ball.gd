53c30d604f3d6bbdc4d233dc0a507895c7d51971
rafaelsilva81 <rafaelgaldinosilva81@gmail.com>
2022-06-07 17:26:01 -0300

    Continunando lógica de jogo

--- Ball.gd
+++ Ball.gd
@@ -2,13 +2,10 @@ extends SGArea2D
 
 onready var collision_shape = $SGCollisionShape2D
 
-const MOVEMENT_SPEED := 65536*2
+const MOVEMENT_SPEED := 65536*4
 var speed = MOVEMENT_SPEED
 
-const MOVEMENT_FRAMES := 60
- 
 var vector: SGFixedVector2
-var frame_count := MOVEMENT_FRAMES
 var last_overlapping_bodies: Array
 
 func _ready() -> void:
@@ -18,21 +15,27 @@ func _network_process(input: Dictionary) -> void:
 	fixed_position.iadd(vector)
 	var overlapping_bodies = get_overlapping_bodies(true)
 	
+			
 	for body in overlapping_bodies:
-		if(body.name == "ServerPlayer" or body.name == "ClientPlayer"):
-			new = SGFixed.vector2(-fixed_position.x, )
-		
+		if (!body in last_overlapping_bodies):
+			if(body.name == "ServerPlayer" or body.name == "ClientPlayer"):
+				vector.x = -vector.x
+	
+	last_overlapping_bodies = overlapping_bodies
+	
 func _save_state() -> Dictionary:
 	var state:= {
 		speed = speed,
-		fixed_position_x = fixed_position_x
+		#fixed_position_x = fixed_position_x,
+		vector = vector
 	}
-	if fixed_position_y != 0:
-		state['fixed_position_y'] = fixed_position_y
+	#if fixed_position_y != 0:
+		#state['fixed_position_y'] = fixed_position_y
 	return state
 
 func _load_state(state: Dictionary) -> void:
-	fixed_position_x = state['fixed_position_x']
-	fixed_position_y = state['fixed_position_y']
+	#fixed_position_x = state['fixed_position_x']
+	#fixed_position_y = state['fixed_position_y']
 	speed = state['speed']
+	vector = state['vector']
 	sync_to_physics_engine()
